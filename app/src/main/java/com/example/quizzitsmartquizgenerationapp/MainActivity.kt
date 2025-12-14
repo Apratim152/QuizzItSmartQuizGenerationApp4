f757bd0f563ea0666c06a05d38b14719c7596a63
Apratim152 <madhuracrj05@gmail.com>
2025-12-14 22:55:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizzitsmartquizgenerationapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.quizzit
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find button
+        val createQuizBtn = findViewById<Button>(R.id.btnCreateQuiz)
+
+        // On click → go to LoginActivity
+        createQuizBtn.setOnClickListener {
+            val intent = Intent(this, LoginCredentials::class.java)
+            startActivity(intent)
+        }
+    }
+}
